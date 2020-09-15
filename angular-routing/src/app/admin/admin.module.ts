13cc942f6d61b99d8f822390746116e00493cb7b
Brijesh Kumar Singh <brij10718@gmail.com>
2020-09-15 22:43:11 +0530

    Completed generating routing features

---
+++ angular-routing/src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { UsersComponent } from './users/users.component';
+import { EmailBlastComponent } from './email-blast/email-blast.component';
+
+
+@NgModule({
+  declarations: [AdminComponent,  UsersComponent, EmailBlastComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }

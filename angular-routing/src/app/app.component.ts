ce1fdbfd9ef61dfb41691e1f19e6f8ed14388dc9
Brijesh Kumar Singh <brij10718@gmail.com>
2020-09-16 23:58:25 +0530

    Completed the tooling feature

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LoggerService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-routing';
+
+  constructor(logger: LoggerService) {
+    logger.log('Hello world!')
+
+  }
 }
